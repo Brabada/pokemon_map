98f1958bdb471e342550b18865761848b0ab6384
Brabada <freemindobject@gmail.com>
2022-11-10 01:37:25 +0300

    Added english and japanese titles to pokemon card

--- pokemon_entities/views.py
+++ pokemon_entities/views.py
@@ -80,8 +80,8 @@ def show_pokemon(request, pokemon_id):
     pokemon_card = {
         "pokemon_id": pokemon.id,
         "title_ru": pokemon.title,
-        "title_en": "",
-        "title_jp": "",
+        "title_en": pokemon.title_en,
+        "title_jp": pokemon.title_jp,
         "description": pokemon.description,
         "img_url": request.build_absolute_uri(pokemon.image.url),
         "next_evolution": {},
