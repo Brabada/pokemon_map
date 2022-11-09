1748d3051b88dfa677407f1d29df9c2566021d80
Brabada <freemindobject@gmail.com>
2022-11-10 01:24:06 +0300

    Added field description for pokemon card

--- pokemon_entities/views.py
+++ pokemon_entities/views.py
@@ -82,7 +82,7 @@ def show_pokemon(request, pokemon_id):
         "title_ru": pokemon.title,
         "title_en": "",
         "title_jp": "",
-        "description": "",
+        "description": pokemon.description,
         "img_url": request.build_absolute_uri(pokemon.image.url),
         "next_evolution": {},
         "previous_evolution": {},
