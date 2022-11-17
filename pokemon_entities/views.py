db0c0fcfdbf1a6a25a1c8cef4c90bae45aafdf64
Brabada <freemindobject@gmail.com>
2022-11-17 22:19:58 +0300

    Change get to get_or_404 in view

--- pokemon_entities/views.py
+++ pokemon_entities/views.py
@@ -1,7 +1,6 @@
 import folium
 
-from django.http import HttpResponseNotFound
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 
 from .models import Pokemon, PokemonEntity
@@ -65,10 +64,7 @@ def show_all_pokemons(request):
 
 
 def show_pokemon(request, pokemon_id):
-    try:
-        pokemon = Pokemon.objects.get(id=pokemon_id)
-    except Pokemon.DoesNotExist:
-        return HttpResponseNotFound('<h1>Такой покемон не найден</h1>')
+    pokemon = get_object_or_404(Pokemon, id=pokemon_id)
 
     localtime_now = timezone.localtime()
     pokemon_entities = PokemonEntity.objects.filter(
